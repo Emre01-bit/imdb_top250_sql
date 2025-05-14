2ae6858ee75e2f38c9f3e4ff1630f7f6410aa482
Emre01-bit <emresama134@outlook.de>
2025-05-14 18:27:52 +0200

    Add create_table.sql to define table structure

---
+++ schema/create_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE movies(
+   rank INTEGER,
+   name TEXT,
+   year INTEGER,
+   rating FLOAT,
+   genre TEXT,
+   certificate TEXT,
+   run_time TEXT,
+   tagline TEXT,
+   budget TEXT,
+   box_office TEXT,
+   casts TEXT,
+   directors TEXT,
+   writers TEXT
+);
